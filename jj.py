6ab9120a6fe5edf7ed0f0857df697ad3a5cddd35
sharan kumar <bssharankumar28920@gmail.com>
2025-04-29 13:45:19 +0530

    printing hello world

---
+++ jj.py
@@ -0,0 +1,9 @@
+class nigga:
+    def __init__(self,name,sec,marks) :
+        self.name=name
+        self.sec=sec
+        self.marks=marks
+    def printable(self):
+        print(f"My name is {self.name} and im in {self.sec} has got {self.marks} marks")
+nigger=nigga(name="sharan", sec="A",marks="100")
+nigger.printable()
