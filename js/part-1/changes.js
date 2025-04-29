2a03dc317c4b7aa066d9afd5b836c056289eb7f7
sharan kumar <bssharankumar28920@gmail.com>
2025-04-29 14:17:02 +0530

    changes done

---
+++ js/part-1/changes.js
@@ -0,0 +1,19 @@
+// declaring
+
+let NiggerGamer="NiggaMan"
+console.log(NiggerGamer);
+
+// updating the variable
+
+NiggerGamer="NiggiWoman"
+console.log(NiggerGamer);
+
+// trying same with const 
+
+try {
+    const niggi="hi"
+    niggi="hello"
+} catch (error) {
+    console.log("const can't be changed");
+    
+}
