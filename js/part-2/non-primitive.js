e65848a359ad40913e11ad43136f1bc945364b91
sharan kumar <bssharankumar28920@gmail.com>
2025-05-01 15:25:38 +0530

    non primitve is done

---
+++ js/part-2/non-primitive.js
@@ -0,0 +1,24 @@
+let obj ={
+    fname:"sharan",
+    lname:"kumar"
+}
+// console.log(typeof(obj));
+let x=obj.fname
+console.log(x);
+
+obj.fname="saran"
+let y = obj.fname;
+
+console.log(y);
+
+console.log(obj);
+
+
+let arr=[]
+arr.push(7)
+arr.push(7)
+arr.push(7)
+console.log(arr);
+
+console.log(arr[2]);
+
