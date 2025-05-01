1abd9fd1ec8f7344b03effedb8895b3dc20d8d4d
sharan kumar <bssharankumar28920@gmail.com>
2025-05-01 14:44:51 +0530

    primitive datatypes has been learnt

---
+++ js/part-2/primitves.js
@@ -0,0 +1,30 @@
+// number 
+
+let balance =69
+console.log(balance);
+console.log(typeof(balance));
+
+
+let var1=67
+let var2 = new Number(68);
+console.log(var2);
+// console.log(typeof (var2)); doesnt need this
+
+const nigga = new Boolean(false);
+console.log(nigga);
+
+// string 
+function ass(str) {
+    return str.split('').reverse().join('');
+}
+console.log(ass("hii"));
+
+let score=50
+let tim_david=`hes hit an ${score} in last match`
+let rajat=`hes hit an ${score} in last match`
+
+console.log(tim_david);
+console.log(rajat);
+
+let sym=Symbol()
+
