a3d0483cc0ad86ca8eaf3946d329b483bcce6daf
sharan kumar <bssharankumar28920@gmail.com>
2025-05-05 09:21:12 +0530

    conditionals done

---
+++ js/part-3/conditions.js
@@ -0,0 +1,35 @@
+let a=58
+let b=8
+
+if(a>b){
+    // console.log( "a is bigger");
+    
+    
+}
+
+// checking if it is a number or not 
+
+let val = 44
+if(typeof val === "number") {
+    // console.log("it is");
+    
+}else{
+    console.log("not a number");
+    
+}
+
+// checking bool is true or not 
+
+let isit =true 
+if (isit) {
+    // console.log("true it is");
+    
+}
+
+// checking if the array is empty or not
+
+let arr=[]
+if(arr.length === 0){
+    console.log("empty");
+    
+}
