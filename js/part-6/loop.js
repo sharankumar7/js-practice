7dab1a36c8e6b97b3f8de82b76d0a0e14520ae86
Sharan kumar <bssharankumar28920@gmail.com>
2025-05-06 15:10:12 +0530

    loops were practiced

--- js/part-6/loop.js
+++ js/part-6/loop.js
@@ -1,8 +1,63 @@
 // 1.sum of numbers from 1 to 5.
-let sum=0
-let i=1
-while (i<=5){
-    sum+=i
-    i++
+// let sum=0
+// let i=1
+// while (i<=5){
+//     sum+=i
+//     i++
+// }
+// console.log(sum);
+
+// 2.push ele in arr using while loop
+
+// let countdown = [];
+// let i = 5;
+
+// while (i > 0) {
+//   countdown.push(i);
+//   i--;
+// }
+// console.log(countdown);
+
+// 3.push ele in array with prompt
+
+// let teacollection = [];
+// let tea;
+// do {
+//   tea = prompt("enter values");
+
+//   if (tea !== "stop") {
+//     teacollection.push(tea);
+//   }
+// } while (tea !== "stop");
+// console.log(teacollection);
+
+// 4.
+
+// let total = 0;
+// let i = 1;
+// do {
+//   total += k;
+//   k++;
+// } while (i <= 3);
+
+// 5.
+
+// let multipliednums = [];
+// let numbers = [2, 4, 6];
+
+// for (let i = 0; i < numbers.length; i++) {
+//   takenumber = numbers[i] * 2;
+//   multipliednums.push(takenumber);
+// }
+// console.log(multipliednums);
+
+// 6.
+
+let cities = ["paris", "ny", "tokyo", "london"];
+let citylist = [];
+
+for (let i = 0; i < cities.length; i++) {
+  const city = cities[i];
+  citylist.push(city);
 }
-console.log(sum);
+console.log(citylist);
