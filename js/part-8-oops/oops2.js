88c374c6d2e9e13e06290170fcb5b0c052d200bc
Sharan kumar <bssharankumar28920@gmail.com>
2025-06-17 14:25:23 +0530

    nothing much progressed

---
+++ js/part-8-oops/oops2.js
@@ -0,0 +1,9 @@
+// encapsulation
+
+// abstraction
+
+// we use "#" before the var but to use it we have to call it through a function.it becomes private property
+
+// polymorphism
+
+// getters and setters
