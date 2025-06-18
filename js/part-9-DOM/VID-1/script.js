b83027540de4f3ff67414188e5fb7bc273c67644
Sharan kumar <bssharankumar28920@gmail.com>
2025-06-18 14:56:06 +0530

    practiced a bit

--- js/part-9-DOM/VID-1/script.js
+++ js/part-9-DOM/VID-1/script.js
@@ -1,6 +1,5 @@
 console.log("hii");
-let btn = document.getElementById("butt");
-// console.log(btn);
-btn.addEventListener("click", () => {
-  console.log("button is clicked");
-});
+// let btn = document.getElementById("btn").addEventListener("click", function () {
+//   let para = document.getElementById("h11");
+//   para.textContent = "hello";
+// });
